3bc3bae92960e49c8fbd0e01346d6cbcf746724f
danielkropf <danielkropf@uol.com.br>
2016-03-04 10:56:55 -0300

    0.1
    - Movimentação funcionando

---
+++ Assets/Resources/Scripts/Movement2.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement2 : MonoBehaviour {
+
+    public GameObject placedOn;
+	private Color32 color;
+	public bool started;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		placedOn.GetComponent<Movment>().onMe = this.gameObject;
+	}
+
+	public void ChangeColor(bool b)
+	{
+		if (!b)
+		{
+			Debug.Log("clickou");
+			if (placedOn.GetComponent<Movment>().immediatelyRight != null)
+			{
+				placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<SpriteRenderer>().color = new Color32(210, 50, 50, 255);
+				placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<Movment>().incoming = this.gameObject;
+				if (placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<Movment>().immediatelyUp != null)
+				{
+					placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<Movment>().immediatelyUp.GetComponent<SpriteRenderer>().color = new Color32(210, 50, 50, 255);
+					placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<Movment>().immediatelyUp.GetComponent<Movment>().incoming = this.gameObject;
+				}
+				if (placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<Movment>().immediatelyDown != null)
+				{
+					placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<Movment>().immediatelyDown.GetComponent<SpriteRenderer>().color = new Color32(210, 50, 50, 255);
+					placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<Movment>().immediatelyDown.GetComponent<Movment>().incoming = this.gameObject;
+				}
+			}
+			if (placedOn.GetComponent<Movment>().immediatelyDown != null)
+			{
+				placedOn.GetComponent<Movment>().immediatelyDown.GetComponent<SpriteRenderer>().color = new Color32(210, 50, 50, 255);
+				placedOn.GetComponent<Movment>().immediatelyDown.GetComponent<Movment>().incoming = this.gameObject;
+			}
+			if (placedOn.GetComponent<Movment>().immediatelyLeft != null)
+			{
+				placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<SpriteRenderer>().color = new Color32(210, 50, 50, 255);
+				placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<Movment>().incoming = this.gameObject;
+				if (placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<Movment>().immediatelyUp != null)
+				{
+					placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<Movment>().immediatelyUp.GetComponent<SpriteRenderer>().color = new Color32(210, 50, 50, 255);
+					placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<Movment>().immediatelyUp.GetComponent<Movment>().incoming = this.gameObject;
+				}
+				if (placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<Movment>().immediatelyDown != null)
+				{
+					placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<Movment>().immediatelyDown.GetComponent<SpriteRenderer>().color = new Color32(210, 50, 50, 255);
+					placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<Movment>().immediatelyDown.GetComponent<Movment>().incoming = this.gameObject;
+				}
+			}
+			if (placedOn.GetComponent<Movment>().immediatelyUp != null)
+			{
+				placedOn.GetComponent<Movment>().immediatelyUp.GetComponent<SpriteRenderer>().color = new Color32(210, 50, 50, 255);
+				placedOn.GetComponent<Movment>().immediatelyUp.GetComponent<Movment>().incoming = this.gameObject;
+			}
+		}
+		else
+		{
+			Debug.Log("elsei vc");
+			if (placedOn.GetComponent<Movment>().immediatelyRight != null)
+			{
+				placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+				placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<Movment>().incoming = null;
+				if (placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<Movment>().immediatelyUp != null)
+				{
+					placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<Movment>().immediatelyUp.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+					placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<Movment>().immediatelyUp.GetComponent<Movment>().incoming = null;
+				}
+				if (placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<Movment>().immediatelyDown != null)
+				{
+					placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<Movment>().immediatelyDown.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+					placedOn.GetComponent<Movment>().immediatelyRight.GetComponent<Movment>().immediatelyDown.GetComponent<Movment>().incoming = null;
+				}
+			}
+			if (placedOn.GetComponent<Movment>().immediatelyDown != null)
+			{
+				placedOn.GetComponent<Movment>().immediatelyDown.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+				placedOn.GetComponent<Movment>().immediatelyDown.GetComponent<Movment>().incoming = null;
+			}
+			if (placedOn.GetComponent<Movment>().immediatelyLeft != null)
+			{
+				placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+				placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<Movment>().incoming = null;
+				if (placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<Movment>().immediatelyUp != null)
+				{
+					placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<Movment>().immediatelyUp.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+					placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<Movment>().immediatelyUp.GetComponent<Movment>().incoming = null;
+				}
+				if (placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<Movment>().immediatelyDown != null)
+				{
+					placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<Movment>().immediatelyDown.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+					placedOn.GetComponent<Movment>().immediatelyLeft.GetComponent<Movment>().immediatelyDown.GetComponent<Movment>().incoming = null;
+				}
+			}
+			if (placedOn.GetComponent<Movment>().immediatelyUp != null)
+			{
+				placedOn.GetComponent<Movment>().immediatelyUp.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+				placedOn.GetComponent<Movment>().immediatelyUp.GetComponent<Movment>().incoming = null;
+			}
+		}
+	}
+
+    void OnMouseUp()
+	{
+		ChangeColor(started);
+		started = !started;
+	}
+}
---
+++ Assets/Resources/Scripts/Movment.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movment : MonoBehaviour {
+
+    public GameObject immediatelyRight;
+    public GameObject immediatelyLeft;
+    public GameObject immediatelyUp;
+    public GameObject immediatelyDown;
+	public GameObject onMe;
+	public GameObject incoming;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUp() {
+		if (incoming != null)
+		{
+			GameObject temp = incoming;
+			temp.GetComponent<Movement2>().ChangeColor(true);
+			temp.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, temp.transform.position.z);
+			temp.GetComponent<Movement2>().placedOn.GetComponent<Movment>().onMe = null;
+			temp.GetComponent<Movement2>().placedOn = this.gameObject;
+			temp.GetComponent<Movement2>().started = false;
+			onMe = incoming;
+			incoming = null;
+			temp = null;
+		}
+	}
+}
