fb23a0dedd524abd98924db6e7b08667e65a274a
danielkropf <danielkropf@uol.com.br>
2016-10-18 10:55:07 -0200

    1.4

--- Assets/Resources/Scripts/Menu/ChooseMenu.cs
+++ Assets/Resources/Scripts/Menu/ChooseMenu.cs
@@ -1,38 +1,79 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ChooseMenu : MonoBehaviour {
 
 
 	int pChosing;
+	public GameObject[] roteiroPlayers = new GameObject[2];
+	public GameObject[] multimidiaPlayers = new GameObject[2];
+	public GameObject[] programacaoPlayers = new GameObject[2];
+	public GameObject readyBtn = new GameObject();
+	public GameObject noBtn = new GameObject();
 
 
 	void Start () {
 		pChosing = 1;
+		roteiroPlayers[0].SetActive(false);
+		roteiroPlayers[1].SetActive(false);
+		multimidiaPlayers[0].SetActive(false);
+		multimidiaPlayers[1].SetActive(false);
+		programacaoPlayers[0].SetActive(false);
+		programacaoPlayers[1].SetActive(false);
+		readyBtn.GetComponent<Button>().interactable = false;
+		noBtn.GetComponent<Button>().interactable = false;
 	}
 
 	public void Click(string n)
 	{
-		switch(n)
+		if (pChosing < 3)
 		{
-			case "Rolthay-ru":
-				PlayerPrefs.SetInt("P"+pChosing,1);
-				pChosing++;
-				break;
-			case "Múhl-Teem-Idhia":
-				PlayerPrefs.SetInt("P" + pChosing, 2);
-				pChosing++;
-				break;
-			case "Prohgam Mason":
-				PlayerPrefs.SetInt("P" + pChosing, 3);
-				pChosing++;
-				break;
+			switch (n)
+			{
+				case "Rolthay-ru":
+					PlayerPrefs.SetInt("P" + pChosing, 1);
+					roteiroPlayers[pChosing - 1].SetActive(true);
+					pChosing++;
+					noBtn.GetComponent<Button>().interactable = true;
+					break;
+				case "Múhl-Teem-Idhia":
+					PlayerPrefs.SetInt("P" + pChosing, 2);
+					multimidiaPlayers[pChosing - 1].SetActive(true);
+					pChosing++;
+					noBtn.GetComponent<Button>().interactable = true;
+					break;
+				case "Prohgam Mason":
+					PlayerPrefs.SetInt("P" + pChosing, 3);
+					programacaoPlayers[pChosing - 1].SetActive(true);
+					pChosing++;
+					noBtn.GetComponent<Button>().interactable = true;
+					break;
+			}
 		}
 
-		if(pChosing > 2)
+		if(pChosing == 3)
 		{
-			Application.LoadLevel("Jogo");
+			readyBtn.GetComponent<Button>().interactable = true;
 		}
+	}
+
+	public void ready()
+	{
+		Application.LoadLevel("Jogo");
+	}
+
+	public void no()
+	{
+		pChosing = 1;
+		roteiroPlayers[0].SetActive(false);
+		roteiroPlayers[1].SetActive(false);
+		multimidiaPlayers[0].SetActive(false);
+		multimidiaPlayers[1].SetActive(false);
+		programacaoPlayers[0].SetActive(false);
+		programacaoPlayers[1].SetActive(false);
+		readyBtn.GetComponent<Button>().interactable = false;
+		noBtn.GetComponent<Button>().interactable = false;
 	}
 
 
